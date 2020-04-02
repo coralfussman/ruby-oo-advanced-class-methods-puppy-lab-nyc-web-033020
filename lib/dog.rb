1071bf1dd9569efbab05da90095c1a5e62247ad0
Coral Fussman <coralfussman@gmail.com>
2020-04-02 02:21:26 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+attr_reader = :name
+@@all_dogs = []
+
+    def initialize(name)
+        @name = name
+        Dog.all << self
+
+    end
+
+
+def self.all 
+    @@all_dogs
+end
+
+
+
+end
